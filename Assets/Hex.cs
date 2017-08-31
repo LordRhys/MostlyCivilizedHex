76e52e55e09f6506532459af84f9ccd974fb6aab
LordRhys <lordrhystower@gmail.com>
2017-08-31 17:39:35 -0500

    Initial Commit, Includes HexPrefab and initial HexMap, can be used as base for other Herx Map Games

---
+++ Assets/Hex.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Hex class defines the grid position, world space position, size,
+/// neighbors, etc... of a Hex Tile. However, it does NOT interact with
+/// Unity directly in any way.
+/// </summary>
+public class Hex {
+
+  public Hex(int q, int r) {
+    this.Q = q;
+    this.R = r;
+    this.S = -(q + r);
+  }
+
+  public readonly int Q; // Column
+  public readonly int R; // Row
+  public readonly int S; // Q + R + S = 0 => S = - (Q + R)
+
+  static readonly float WIDTH_MULTIPLIER = Mathf.Sqrt( 3 ) / 2;
+
+  /// <summary>
+  /// Returns the world space position of this Hex
+  /// </summary>
+  /// <returns></returns>
+  public Vector3 Position() {
+    float radius = 1f;
+    float height = radius * 2;
+    float width = WIDTH_MULTIPLIER * height;
+
+    float vert = height * 0.75f;
+    float horiz = width;
+
+    return new Vector3(
+            horiz * (this.Q + this.R/2f),
+            0,
+            vert * this.R
+          ); 
+          
+  }
+}
---
+++ Assets/HexMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+    GenerateMap();
+  }
+
+  public GameObject HexPrefab;
+  
+  public void GenerateMap() {
+    for (int column = 0; column < 10; column++)
+    {
+      for (int row = 0; row < 10; row++)
+      {
+        // Instantiate a Hex
+        Hex h = new Hex( column, row );
+
+        Instantiate( 
+          HexPrefab,
+          h.Position(),
+          Quaternion.identity,
+          this.transform
+        );
+      }
+    }
+  }
+}
